0d94f1c0f6ae85a053cdeb4f8c12e2942534fb9c
kenibenf <davide23422@gmail.com>
2023-04-15 17:55:48 +0200

    Fix Shift
    Con il cambio del metodo di input non funzionava lo shift, cambiato un paio di cose e ora funziona.
    
    To Do: implementare il nuovo sistema di input anche per interagire con npc e stoppare il gioco

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,11 +10,16 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private bool isMove;
     public float shiftDownSpeed;
+    private InputAction moveAction;
+    private InputAction shiftAction;
+    private float shiftt = 1; // forse c'è un modo più efficiente per farlo, ma cosi funziona
 
     Vector2 oldPos;
 
     private void Start()
     {
+        moveAction = GetComponent<PlayerInput>().actions["Move"];
+        shiftAction = GetComponent<PlayerInput>().actions["Shift"];
         // GetComponent "preleva" il componente attaccato all'oggetto, in questo caso preleva il componente BoxCollider2D dalla sprite del player_0
         rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true;
@@ -23,25 +28,34 @@ public class PlayerMovement : MonoBehaviour
     
     private void Update()
     {
-        speed = PlayerState.speed;
+
+        //PER MUOVERSI
+        moveDelta = moveAction.ReadValue<Vector2>(); // prende il valore dall'azione Move, che è passato tramite l'input system di unity
+        x = moveDelta.x; 
+        y = moveDelta.y;
+
+        //PER SHIFTARE
+        shiftAction.performed += OnShift; // se e' stato premuto shift, allora esegui OnShift()
+        shiftAction.canceled += OnShiftReleased; // se e' stato rilasciato shift, allora esegui OnShiftReleased()
+
+
+        speed = PlayerState.speed * shiftt;
         rb.velocity = moveDelta * speed;
         this.Animate();
     }
-    
-    public void OnShift(){
-        speed = speed * shiftDownSpeed;
-
+    // sinceramente il parametro Callbackcontext non so cosa sia, ma serviva affinche funzionasse.
+    private void OnShift(InputAction.CallbackContext contex){ 
+        shiftt = shiftDownSpeed; //setto la velocità a 0.5
     }
+
+    private void OnShiftReleased(InputAction.CallbackContext contex){
+        shiftt = 1; // una volta rilasciato shift, risetto la velocità a 1
+    }
+    
     /* 
         funzione chiamata ogni volta che l'utente preme wasd/freccette 
         (se si vuole cambiare combinazione di tasti o aggiungerne altri si fa dal componente Player Input del Player)
     */
-    public void OnMove(InputValue value)
-    {
-        moveDelta = value.Get<Vector2>(); // prende il valore da inputValue, che è passato tramite l'input system di unity
-        x = moveDelta.x;
-        y = moveDelta.y;
-    }
 
     private void Animate()
     {
